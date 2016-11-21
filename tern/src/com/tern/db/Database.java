82e37d2acd870a4b5bfab57eb11dcea192af21aa
fancimage <qiao_xf@163.com>
2016-11-21 23:58:54 +0800

    modify:database.java,remove MethodNotFoundException

--- tern/src/com/tern/db/Database.java
+++ tern/src/com/tern/db/Database.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.el.MethodNotFoundException;
 import javax.sql.DataSource;
 import com.tern.dao.NamedValue;
 import com.tern.util.Convert;
